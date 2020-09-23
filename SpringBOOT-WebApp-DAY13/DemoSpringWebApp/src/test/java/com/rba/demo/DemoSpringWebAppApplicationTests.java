2a7130becfe1ffd0ab391a4ee0e58217771daf24
sbinjv@gmail.com <Java@#19682>
2020-09-23 16:19:29 +0530

    Spring-Boot-Day13
    Spring-Boot-Day13

---
+++ SpringBOOT-WebApp-DAY13/DemoSpringWebApp/src/test/java/com/rba/demo/DemoSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
